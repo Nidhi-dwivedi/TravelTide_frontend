7a069719d38094be5cade7c0ed234b6c5a0432a7
Nidhi-dwivedi <nidhirani1009@gmail.com>
2024-08-12 02:52:00 +0530

    first commit

---
+++ src/components/common/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Navbar.css"
+
+const Navbar = ({userInfo, setUserInfo}) => {
+  return (
+    <div id='navbar-container'>
+        <div>
+          <div>
+            <Link to="/"><span>TravelTide.in</span></Link>
+          </div>
+          <div id='nav-links'>
+            <Link to="/reservation"><span>RESERVATION</span></Link>  
+            { userInfo.isSignin && <span>Hi, {userInfo.name}</span> }
+            { !userInfo.isSignin &&
+              <>
+               <Link to="/signin"><span>SIGNIN</span></Link>
+               <Link to="/signin"><span>SIGNUP</span></Link>
+              </>  
+            }
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
